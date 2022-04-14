1a40da24a229eda1bdd72aaa767252e024f0b67b
AndrewGuerin <c00243040@itcarlow.ie>
2022-04-14 14:38:29 +0100

    Readded Display Account Wallet Address

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 import * as fcl from "@onflow/fcl";
+import {useState, useEffect} from 'react';
 
 
 //essential for connection to testnet blockchain
@@ -9,7 +10,14 @@ fcl.config()
     .put("discovery.wallet", "https://fcl-discovery.onflow.org/testnet/authn")
 
 function App() {
+  const [user, setUser] = useState();
 
+  //sets the user variable to the user that just logged in
+  useEffect(() => {
+    fcl.currentUser().subscribe(setUser);
+  }, [])
+
+  //Login in the user
   const login = () => {
     fcl.authenticate();
   }
@@ -17,8 +25,9 @@ function App() {
 
   return (
     <div className="App">
-        <h1>Hello look at this</h1>
+        <h1>Account Address: {user && user.addr ? user.addr : ''}</h1>
         <button onClick={() => login()}>Log In Function</button>
+
     </div>
   );
 }
