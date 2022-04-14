2700029035147ea4d8672c9909ecbc95ffe80af9
AndrewGuerin <c00243040@itcarlow.ie>
2022-04-14 15:47:40 +0100

    Altered MintNFT Contract

--- src/contracts/transactions/mint_NFT.js
+++ src/contracts/transactions/mint_NFT.js
@@ -1,12 +1,12 @@
-export default mintNFT = `
+export const mintNFT = `
 import MyNFT from x48d98a1c02880291
 
-transaction(ipfsHash: String, metadata: {String: String}) {
+transaction(ipfsHash: String, name: String) {
 
   prepare(acct: AuthAccount) {
     let collection = acct.borrow<&MyNFT.Collection>(from: /storage/MyNFTCollection)
         ?? panic("This collection does not exist here")
-    let nft <- MyNFT.createToken(ipfsHash: ipfsHash, metadata: metadata)
+    let nft <- MyNFT.createToken(ipfsHash: ipfsHash, metadata: {"name": name})
 
     collection.deposit(token: <- nft)
   }
