78a15c4e5352b8099513030abb8a6011cab7fe1f
AndrewGuerin <c00243040@itcarlow.ie>
2022-04-19 20:50:48 +0100

    Display collection of NFT's

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
   }, [])
 
   //Login in the user
-  const login = () => {
+  const logIn = () => {
     fcl.authenticate();
   }
 
@@ -88,15 +88,15 @@ function App() {
   return (
     <div className="App">
         <h1>Account Address: {user && user.addr ? user.addr : ''}</h1>
-        <button onClick={() => login()}>Log In Function</button>
+        <button onClick={() => logIn()}>Log In Function</button>
         <button onClick={() => fcl.unauthenticate()}>Log Out Function</button>
         <button onClick={() => setupUser()}>Setup User</button>
 
 
         <div>
-            <input type="text" onChange={(e) => setNameOfNFT(e.target.value)} />
-            <input type="file" onChange={(e) => setFile(e.target.files[0])}/>
-            <button onClick={() => mint()}>Mint</button>
+          <input type="text" onChange={(e) => setNameOfNFT(e.target.value)} />
+          <input type="file" onChange={(e) => setFile(e.target.files[0])}/>
+          <button onClick={() => mint()}>Mint</button>
         </div>
 
         <Collection address={user.addr}></Collection>
--- src/Collection.js
+++ src/Collection.js
@@ -3,13 +3,37 @@ import './App.css';
 import * as fcl from "@onflow/fcl";
 import * as t from "@onflow/types";
 import {useState, useEffect} from 'react';
+import {getNFTsScript} from "./contracts/scripts/get_nfts.js"
 
 
 function Collection(props) {
+  const [nfts, setNFTs] = useState([]);
+
+  useEffect(() => {
+    getUserNFTs();
+  }, [])
+
+  const getUserNFTs = async () => {
+      const result = await fcl.send([
+          fcl.script(getNFTsScript),
+          fcl.args([
+              fcl.arg(props.address, t.Address)
+          ])
+      ]).then(fcl.decode);
+
+      console.log(result);
+      setNFTs(result);
+  }
 
   return (
-    <div className="App">
-        <h1>{props.address}</h1>
+    <div style={{backgroundColor: 'lightgreen'}}>
+      {nfts.map(nft => (
+            <div>
+                <h1>{nft.id}</h1>
+                <h1>{nft.ipfsHash}</h1>
+                <h1>{nft.metadata.name}</h1>
+            </div>
+      ))}
     </div>
   );
 }
