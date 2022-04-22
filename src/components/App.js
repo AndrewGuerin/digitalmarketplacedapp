dfc47dc083a6f97139cb99c6707c3a9400399e88
AndrewGuerin <c00243040@itcarlow.ie>
2022-04-22 19:41:37 +0100

    Altered main app class to allow routes

---
+++ src/components/App.js
@@ -0,0 +1,36 @@
+
+import React from "react";
+
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
+import {AuthProvider} from "../contexts/AuthContext";
+
+import Chats from "./Chats";
+import ListingScreen from "./ListingScreen";
+import Login from "./Login";
+import MarketScreenOne from "./MarketScreenOne"
+import NFTMint from "./NFTMint"
+import SearchScreen from "./SearchScreen";
+
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'Avenir'}}>
+      <Router>
+        <AuthProvider>
+          <Switch>
+            <Route path="/chats" component={Chats} />
+            <Route path="/MarketScreenOne" component={MarketScreenOne} />
+            <Route path="/NFTMint" component={NFTMint} />
+            <Route path="/SearchScreen" component={SearchScreen} />
+            <Route path="/ListingScreen" component={ListingScreen} />
+            <Route path="/" component={Login} /> 
+
+          </Switch>
+        </AuthProvider>
+      </Router>
+    </div>
+  )
+}
+
+export default App;
